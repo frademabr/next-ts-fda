938578dfd504dac6f4986a313e98ef4d36cbbf15
frademabr <yasmin@fradema.com.br>
2025-01-15 08:44:35 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,3 +3,10 @@ module.exports = {
     dirs: ["pages", "utils"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
   },
 };
+
+module.exports = {
+  extends: [
+    //...
+    "plugin:@next/next/recommended",
+  ],
+};
