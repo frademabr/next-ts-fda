856bcfd438a61b82c2cdbd3a5950fa95c8f09c08
frademabr <yasmin@fradema.com.br>
2025-01-20 21:07:50 -0300

    frademao

--- src/sections/_filiais/services/marketing-services-how-it-work.tsx
+++ src/sections/_filiais/services/marketing-services-how-it-work.tsx
@@ -16,126 +16,40 @@ import { CONFIG } from "src/global-config";
 
 // ----------------------------------------------------------------------
 
-const COLORS = ["primary", "secondary", "warning", "success"] as const;
-
-const TIMELINES = [
-  {
-    title: "Planning",
-    description:
-      "Curabitur ullamcorper ultricies nisi. Praesent nonummy mi in odio. Donec mollis hendrerit risus.",
-  },
-  {
-    title: "Research",
-    description:
-      "Curabitur ullamcorper ultricies nisi. Praesent nonummy mi in odio. Donec mollis hendrerit risus.",
-  },
-  {
-    title: "Optimizing",
-    description:
-      "Curabitur ullamcorper ultricies nisi. Praesent nonummy mi in odio. Donec mollis hendrerit risus.",
-  },
-  {
-    title: "Results",
-    description:
-      "Curabitur ullamcorper ultricies nisi. Praesent nonummy mi in odio. Donec mollis hendrerit risus.",
-  },
-];
-
 // ----------------------------------------------------------------------
 
 export function MarketingServicesHowItWork({ sx, ...other }: BoxProps) {
   return (
-    <Box
-      component="section"
-      sx={[
-        (theme) => ({
-          ...theme.mixins.bgGradient({
-            images: [
-              `linear-gradient(to bottom, ${varAlpha(
-                theme.vars.palette.common.blackChannel,
-                0
-              )}, ${varAlpha(theme.vars.palette.common.blackChannel, 0)})`,
-              `url(${CONFIG.assetsDir}/assets/images/home/skynight.jpg)`,
-            ],
-          }),
-          color: "common.white",
-          py: { xs: 10, md: 15 },
-          height: "960px",
-        }),
-        ...(Array.isArray(sx) ? sx : [sx]),
-      ]}
-      {...other}
-    >
-      <Container className="-mb-12">
-        <div className="">
-          <Typography
-            className="flex flex-1 text-pretty mx-auto justify-center mt-30 text-9xl -mb-20 text-opacity-80"
-            sx={{
-              display: "flex",
-              alignContent: "center",
-              fontSize: "172px",
-              fontWeight: "bold",
-              marginBottom: "-150px",
-              paddingTop: "200px",
-              mixBlendMode: "soft-light",
-              opacity: "1",
+    <div>
+      <div className="page-header clear-filter">
+        <Box
+          component="section"
+          className="rellax-header rellax-header-sky"
+          sx={{ marginTop: "40px", paddingBottom: "-60px" }}
+        >
+          <div
+            className="bg-bottom bg-cover -mb-80"
+            style={{
+              backgroundImage: `url(${CONFIG.assetsDir}/assets/images/home/skynight.jpg)`,
+
+              height: "560px",
             }}
-          >
-            FRADEMA
-          </Typography>
-        </div>
-      </Container>
+          />
 
-      <Box
-        component="section"
-        sx={[
-          (theme) => ({
-            ...theme.mixins.bgGradient({
-              images: [
-                `linear-gradient(to bottom, ${varAlpha(
-                  theme.vars.palette.common.blackChannel,
-                  0
-                )}, ${varAlpha(theme.vars.palette.common.blackChannel, 0.3)})`,
-                `url(${CONFIG.assetsDir}/assets/images/home/nyblue.png)`,
-              ],
-            }),
-            color: "common.white",
-            py: { xs: 25, md: 30 },
-            marginTop: "100px",
-            // width: "900px",
-          }),
-          ...(Array.isArray(sx) ? sx : [sx]),
-        ]}
-        {...other}
-      />
-
-      {/* <Timeline position="alternate">
-          {TIMELINES.map((value, index) => (
-            <TimelineItem key={value.title}>
-              <TimelineSeparator>
-                <TimelineDot color={COLORS[index]} />
-                <TimelineConnector />
-              </TimelineSeparator>
-
-              <TimelineContent sx={{ pb: { xs: 3, md: 5 } }}>
-                <Typography variant="overline" sx={{ color: `${COLORS[index]}.main` }}>
-                  STEP {index + 1}
-                </Typography>
-
-                <Typography variant="h5" component="h6" sx={{ mt: 0.5, mb: 1 }}>
-                  {value.title}
-                </Typography>
-
-                <Typography
-                  variant="body2"
-                  sx={{ opacity: 0.72, maxWidth: { md: 360 }, ...(index % 2 && { ml: 'auto' }) }}
-                >
-                  {value.description}
-                </Typography>
-              </TimelineContent>
-            </TimelineItem>
-          ))}
-        </Timeline> */}
-    </Box>
+          <div className="-mb-60">
+            <h1 className="ml-5 h1-seo">FRADEMA</h1>
+          </div>
+          <div className="mt-5">
+            <div
+              className="bg-bottom bg-cover "
+              style={{
+                backgroundImage: `url(${CONFIG.assetsDir}/assets/images/home/nyblue.png)`,
+                height: "666px",
+              }}
+            />
+          </div>
+        </Box>
+      </div>
+    </div>
   );
 }
