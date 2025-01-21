fc561b2fe441e852133cd4a8431f0da956b8fa82
frademabr <yasmin@fradema.com.br>
2025-01-21 11:25:47 -0300

    Update marketing-services-how-it-work.tsx

--- src/sections/_filiais/services/marketing-services-how-it-work.tsx
+++ src/sections/_filiais/services/marketing-services-how-it-work.tsx
@@ -1,16 +1,8 @@
 import type { BoxProps } from "@mui/material/Box";
 
-import { varAlpha } from "minimal-shared/utils";
+// import { varAlpha } from "minimal-shared/utils";
 
 import Box from "@mui/material/Box";
-import Timeline from "@mui/lab/Timeline";
-import TimelineDot from "@mui/lab/TimelineDot";
-import Container from "@mui/material/Container";
-import TimelineItem from "@mui/lab/TimelineItem";
-import Typography from "@mui/material/Typography";
-import TimelineContent from "@mui/lab/TimelineContent";
-import TimelineConnector from "@mui/lab/TimelineConnector";
-import TimelineSeparator from "@mui/lab/TimelineSeparator";
 
 import { CONFIG } from "src/global-config";
 import { m } from "framer-motion";
@@ -21,33 +13,33 @@ import { m } from "framer-motion";
 
 export function MarketingServicesHowItWork({ sx, ...other }: BoxProps) {
   return (
-    <div className="md:max-lg:flex">
-      <div className="">
+    <div className="relative w-full md:max-w-full">
+      <div className="md:max-h-full">
         <Box
           // component=""
-          sx={{ marginTop: "50px", paddingBottom: "-60px" }}
+          sx={{ marginTop: "60px" }}
         >
           <div
-            className="bg-bottom bg-cover shadow-2xl md:shrink-0 -mb-80"
+            className="bg-bottom bg-cover -mb-80"
             style={{
               backgroundImage: `url(${CONFIG.assetsDir}/assets/images/home/skynight.jpg)`,
 
-              height: "560px",
+              height: "600px",
             }}
           />
 
-          <m.div className="-mb-60">
-            <h1 className="ml-5 tracking-tighter h1-seo">FRADEMA</h1>
+          <m.div className="-mb-60 ">
+            <h1 className="ml-5 tracking-tighter text-center text-pretty h1-seo">FRADEMA</h1>
           </m.div>
-          <div className="mt-5">
+          <m.div className="mb-5">
             <div
-              className="bg-bottom bg-cover md:shrink-0"
+              className="bg-cover "
               style={{
                 backgroundImage: `url(${CONFIG.assetsDir}/assets/images/home/nyblue.png)`,
                 height: "666px",
               }}
             />
-          </div>
+          </m.div>
         </Box>
       </div>
     </div>
