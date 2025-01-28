b59d55a4ba620a16ce010c3e0a0f21c44dce39b5
frademabr <yasmin@fradema.com.br>
2025-01-28 08:19:30 -0300

    xl

--- src/sections/_filiais/services/marketing-services-how-it-work.tsx
+++ src/sections/_filiais/services/marketing-services-how-it-work.tsx
@@ -20,19 +20,19 @@ import { Container } from "@mui/material";
 export function MarketingServicesHowItWork({ sx, ...other }: BoxProps) {
   return (
     <section className="">
-      <m.div className=" lg:-mb-10">
+      <m.div className=" lg:-mb-10 xl:py-10 xl:-mt-30">
         <Image className="object-cover" src={bluesky} alt="hero_image1" />
       </m.div>
 
-      <div className="lg:-mt-[240px]">
-        <h1 className=" tracking-tighter text-center lg:text-[220px] md:text-[180px] sm:text-[120px]  font-bold lg:mb-20">
+      <div className="lg:-mt-[240px] xl:-mt-[430px]">
+        <h1 className=" tracking-tighter text-center xl:text-[320px] lg:text-[220px] md:text-[180px] sm:text-[120px]  font-bold lg:mb-20 xl:mb-40">
           FRADEMA
         </h1>
       </div>
 
       <div className="">
         <Image
-          className="object-cover lg:-mt-55 md:-mt-30 sm:-mt-12"
+          className="object-cover lg:-mt-55 xl:-mt-88 md:-mt-30 sm:-mt-12"
           src={nyblue}
           alt="hero_image2"
         />
